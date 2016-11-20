c161228bb4ac20904cce69e9bab3f692234edb15
drkohlipk <drkohlipk@yahoo.com>
2016-11-20 13:11:30 -0800

    Created class for card_deck

--- thanksgivingclasses.py
+++ thanksgivingclasses.py
@@ -1,47 +1,46 @@
 class Character(object):
-  def __init__(self, name, special, luck, beer_stamina, damage)
-      self.special = special
-      self.luck = luck
-      self.damage = damage
-      self.sobriety = 0
-      self.health = 100
-      self.gold = 20
-      self.name = name
-      self.beer_stamina = beer_stamina
-      self.tryptophan = 0
-      self.family = {
-        'dad' : {
-            'health' : 100,
-            'beer_stamina' : 4,
-            'sobriety' : 100,
-            'damage' : 4
-        },
-        'mom' : {
-            'health' : 100,
-            'beer_stamina' : 3,
-            'sobriety' : 100,
-            'damage' : 3,
-        },
-        'bro' : {
-            'health' : 100,
-            'beer_stamina' : 2,
-            'sobriety' : 100,
-            'damage' : 2,
-        },
-        'sis' : {
-            'health' : 100,
-            'beer_stamina' : 2,
-            'sobriety' : 100,
-            'damage' : 2,
-        },
-        'dog' : {
-            'health' : 100,
-            'beer_stamina' : 1,
-            'sobriety' : 100,
-            'damage' : 2,
-        }
-
-      }
+	def __init__(self, name, special, luck, beer_stamina, damage):
+		self.special = special
+		self.luck = luck
+		self.damage = damage
+		self.name = name
+		self.beer_stamina = beer_stamina
+		self.sobriety = 100
+		self.health = 100
+		self.money = 20
+		self.tryptophan = 0
+		self.family = {
+			'dad' : {
+				'health' : 100,
+				'beer_stamina' : 4,
+				'sobriety' : 100,
+				'damage' : 4
+			},
+			'mom' : {
+				'health' : 100,
+				'beer_stamina' : 3,
+				'sobriety' : 100,
+				'damage' : 3,
+			},
+				'bro' : {
+				'health' : 100,
+				'beer_stamina' : 2,
+				'sobriety' : 100,
+				'damage' : 2,
+			},
+				'sis' : {
+				'health' : 100,
+				'beer_stamina' : 2,
+				'sobriety' : 100,
+				'damage' : 2,
+			},
+				'dog' : {
+				'health' : 100,
+				'beer_stamina' : 1,
+				'sobriety' : 100,
+				'damage' : 2,
+			},
+		}
 
 josiah = Character('Josiah', 'Bible', 3, 2, 1)
 samuel = Character('Samuel', 'Drinking', 1, 3, 2)
@@ -50,3 +49,71 @@ isabelle = Character('Isabelle', 'Cats', 1, 2, 3)
 emily = Character('Emily', 'Nun', 3, 1, 2)
 sarah = Character('Sarah', 'Nurse', 2, 3, 1)
 madjack = Character('MadJack', 'Coding Wizard', 4, 4, 4)
+
+class Card_Deck(object):
+	def __init__(self):
+		self.two = {
+			'name' : 'two',
+			'value' : 2,
+			'quantity' : 4,
+		}
+		self.three = {
+			'name' : 'three',
+			'value' : 3,
+			'quantity' : 4,
+		}
+		self.four = {
+			'name' : 'four',
+			'value' : 4,
+			'quantity' : 4,
+		}
+		self.five = {
+			'name' : 'five',
+			'value' : 5,
+			'quantity' : 4,
+		}
+		self.six = {
+			'name' : 'six',
+			'value' : 6,
+			'quantity' : 4,
+		}
+		self.seven = {
+			'name' : 'seven',
+			'value' : 7,
+			'quantity' : 4,
+		}
+		self.eight = {
+			'name' : 'eight',
+			'value' : 8,
+			'quantity' : 4,
+		}
+		self.nine = {
+			'name' : 'nine',
+			'value' : 9,
+			'quantity' : 4,
+		}
+		self.ten = {
+			'name' : 'ten',
+			'value' : 10,
+			'quantity' : 4,
+		}
+		self.jack = {
+			'name' : 'jack',
+			'value' : 10,
+			'quantity' : 4,
+		}
+		self.queen = {
+			'name' : 'queen',
+			'value' : 10,
+			'quantity' : 4,
+		}
+		self.king = {
+			'name' : 'king',
+			'value' : 10,
+			'quantity' : 4,
+		}
+		self.ace = {
+			'name' : 'ace',
+			'value' : 11,
+			'quantity' : 4,
+		}
