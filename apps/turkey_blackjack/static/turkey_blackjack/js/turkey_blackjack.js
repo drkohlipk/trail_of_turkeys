2738bc5c6797bdce4914280f36bc5fcc0a61e27a
drkohlipk <drkohlipk@yahoo.com>
2016-11-20 23:23:32 -0800

    Completed blackjack logic minus split and double down

--- apps/turkey_blackjack/static/turkey_blackjack/js/turkey_blackjack.js
+++ apps/turkey_blackjack/static/turkey_blackjack/js/turkey_blackjack.js
@@ -77,15 +77,15 @@ function CardDeck() {
 	this.rando = function(luck) {
 		var num;
 		if (luck >= 3) {
-			var lucky = Math.Floor(Math.random() * (luck + 1));
+			var lucky = Math.floor(Math.random() * luck);
 			if (lucky < 2) {
 				num = Math.floor(Math.random() * 13);
 			} else {
-				num = Math.floor(Math.random() * 8);
+				num = Math.floor(Math.random() * 7);
 				num += 6;
 			}
 		} else if (luck == 1) {
-			var unlucky = Math.Floor(Math.random() * 4);
+			var unlucky = Math.floor(Math.random() * 3);
 			if (unlucky < 2) {
 				num = Math.floor(Math.random() * 13);
 			} else {
@@ -99,11 +99,9 @@ function CardDeck() {
 	};
 
 	this.deal = function(luck) {
-		luck = luck || 2;
-		var num = this.rando(),
+		var num = this.rando(luck),
         	card = {'face':this.cards[num].name, 'value': this.cards[num].value};
         this.cards[num].quantity -= 1;
-		console.log(luck);
         return card;
 	};
 
@@ -121,28 +119,59 @@ function CardDeck() {
 	};
 }
 
-function Dealer() {
-    this.hand = [];
-
-	this.check = function() {
-		var sum = 0;
-		var bust = false;
-		for (var i = 0; i < this.hand.length; i++) {
-			sum += this.hand[i].value;
-		}
-		if (sum > 21) {
-			bust = true;
-		} else {
-			bust = false;
-		}
-		return sum, bust;
-	};
-}
-
 function Player(money, luck) {
     this.money = money;
     this.luck = luck;
     this.hand = [];
+	this.bust = false;
+	this.sum = 0;
+	this.blackjack = false;
+	this.bet = 5;
+
+	this.check = function() {
+		this.sum = 0;
+		for (var i = 0; i < this.hand.length; i++) {
+			this.sum += this.hand[i].value;
+		}
+		if (this.sum > 21) {
+			if(this.aceCheck()) {
+				this.check();
+			} else {
+				this.bust = true;
+			}
+		} else if (this.sum == 21 && this.hand.length == 2) {
+			this.blackjack = true;
+		} else {
+			this.bust = false;
+		}
+		return this;
+	};
+
+	this.aceCheck = function() {
+		for (var i = 0; i < this.hand.length; i++) {
+			if (this.hand[i].value == 11) {
+				this.hand[i].value = 1;
+				return true;
+			}
+		}
+		return false;
+	};
+
+	this.softCheck = function() {
+		var soft = false;
+		for (var i = 0; i < this.hand.length; i++) {
+			if (this.hand[i].value == 11) {
+				soft = true;
+				break;
+			}
+		}
+		return soft;
+	};
+
+	this.hit = function () {
+		this.hand.push(deck.hit(this.luck));
+	};
+
 }
 
 /*****************end constructor functions*********************/
@@ -156,44 +185,73 @@ var deck,
 function startgame() {
 	deck = new CardDeck();
     player = new Player(money, luck);
-    dealer = new Dealer();
+    dealer = new Player(0, 2);
     return;
 }
 
 function deal() {
     for (var i = 0; i < 4; i++) {
         if (i % 2 === 0) {
-			player.hand.push(deck.deal(luck));
+			player.hand.push(deck.deal(player.luck));
 		} else {
-			dealer.hand.push(deck.deal());
+			dealer.hand.push(deck.deal(dealer.luck));
 		}
+	}
+	dealer.check();
+	player.check();
+	if (dealer.blackjack || player.blackjack) {
+		console.log(end());
 	}
     return;
 }
 
-function check(hand){
-	var sum = 0;
-	var bust = false;
-	for (var i = 0; i < hand.length; i++) {
-		sum += hand[i].value;
+function hit() {
+	player.hit();
+	player.check();
+	if (player.bust) {
+		console.log(end());
 	}
-	if (sum > 21) {
-		aceCheck(hand);
-		bust = true;
-	} else {
-		bust = false;
-	}
-	return sum, bust;
+	return;
 }
 
-function aceCheck(hand) {
-	for (var i = 0; i < hand.length; i++) {
-		if (hand[i].value == 11) {
-			hand[i].value = 1;
-			check(hand);
-			break;
-		}
+function stay() {
+	var soft = false;
+	if (dealer.sum == 17) {
+		soft = dealer.softCheck();
 	}
+	if ((dealer.sum == 17 && soft) || dealer.sum < 17) {
+		dealer.hit();
+		dealer.check();
+		stay();
+	} else {
+		console.log(end());
+	}
+}
+
+function end() {
+	var message;
+	if (player.bust) {
+		message = "Player loses, dealer wins.";
+		player.money -= player.bet;
+	} else if (dealer.bust) {
+		message = "Player wins, dealer loses.";
+		player.money += player.bet;
+	} else if (player.blackjack && !dealer.blackjack) {
+		message = "Winner Winner Turkey dinner!!";
+		player.money += (player.bet * 2);
+	} else if (dealer.blackjack && !player.blackjack) {
+		message = "Ouch!!!  You lose...";
+		player.money -= player.bet;
+	} else if (player.sum > dealer.sum) {
+		message = "Player wins, dealer loses.";
+		player.money += player.bet;
+	} else if (dealer.sum > player.sum) {
+		message = "Player loses, dealer wins.";
+		player.money -= player.bet;
+	} else if (player.sum == dealer.sum) {
+		message = "Push, deal again.";
+	}
+	return message;
 }
 
 /*****************end game functions*********************/
@@ -203,5 +261,6 @@ startgame();
 deal();
 console.log(dealer);
 console.log(player);
-console.log(deck);
-console.log(deck.hit());
+stay();
+console.log(dealer);
+console.log(player);
